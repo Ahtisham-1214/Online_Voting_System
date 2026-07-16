d4aef85ec483469a594ce87ee7dfe5860414b93a
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-16 14:38:18 +0500

    Added functionality to hande InSensitive case

--- src/main/java/voting/system/VotingManagementSystem/entity/ElectionStatus.java
+++ src/main/java/voting/system/VotingManagementSystem/entity/ElectionStatus.java
@@ -1,7 +1,29 @@
 package voting.system.VotingManagementSystem.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import voting.system.VotingManagementSystem.exception.IllegalEnumException;
+
+import java.util.Arrays;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING, with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 public enum ElectionStatus {
     RESULT_PUBLISHED,
     CLOSED,
-    VOTING
+    VOTING,
+    DRAFT,
+    TEST;
+
+    @JsonCreator
+    public static ElectionStatus fromString(String value) {
+        if (value == null || value.isEmpty()) {
+            return null;
+        }
+        try {
+            return ElectionStatus.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalEnumException(value + " is not accepted. Allowed: " +
+                    Arrays.toString(ElectionStatus.values()), ElectionStatus.class.getSimpleName());
+        }
+    }
 }
