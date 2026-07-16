a1ca5a287083669452866addf1a20c74ce84ff8a
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-16 14:39:20 +0500

    global exception class

---
+++ src/main/java/voting/system/VotingManagementSystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package voting.system.VotingManagementSystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        ErrorResponse errorPayload = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                ex.getMessage(),
+                ex.getClassName()
+        );
+        return new ResponseEntity<>(errorPayload, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalEnumException.class)
+    public ResponseEntity<ErrorResponse> handleEnumNotFoundException(IllegalEnumException ex) {
+        ErrorResponse errorPayload = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Bad Argument",
+                ex.getMessage(),
+                ex.getClassName()
+        );
+        return new ResponseEntity<>(errorPayload, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+
+}
