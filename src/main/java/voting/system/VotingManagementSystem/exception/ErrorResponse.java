683eef9ea0dd797d6ce551a728b15f7943ef69f2
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-16 14:38:53 +0500

    Error response DTO

---
+++ src/main/java/voting/system/VotingManagementSystem/exception/ErrorResponse.java
@@ -0,0 +1,25 @@
+package voting.system.VotingManagementSystem.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String className;
+
+    public ErrorResponse(int status, String error, String message, String className) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.className = className;
+    }
+
+}
