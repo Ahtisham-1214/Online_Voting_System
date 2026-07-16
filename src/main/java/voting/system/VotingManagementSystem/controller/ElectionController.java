7462188c3506741d7ae63a986bc53dcfefdcb307
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-16 20:45:15 +0500

    added more methods

--- src/main/java/voting/system/VotingManagementSystem/controller/ElectionController.java
+++ src/main/java/voting/system/VotingManagementSystem/controller/ElectionController.java
@@ -1,6 +1,6 @@
 package voting.system.VotingManagementSystem.controller;
 
-import org.modelmapper.ModelMapper;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,30 +8,50 @@ import org.springframework.web.bind.annotation.*;
 import voting.system.VotingManagementSystem.dto.ElectionRequestDto;
 import voting.system.VotingManagementSystem.dto.ElectionResponseDto;
 import voting.system.VotingManagementSystem.entity.Election;
+import voting.system.VotingManagementSystem.mapper.MyMapper;
 import voting.system.VotingManagementSystem.service.ElectionService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 public class ElectionController {
     private final ElectionService electionService;
-    private final ModelMapper modelMapper;
 
     @Autowired
-    public ElectionController(ElectionService electionService, ModelMapper modelMapper) {
+    private  MyMapper myMapper;
+    @Autowired
+    public ElectionController(ElectionService electionService) {
         this.electionService = electionService;
-        this.modelMapper = modelMapper;
     }
 
     @PostMapping("/elections")
-    public ResponseEntity<ElectionResponseDto> createElection(@RequestBody ElectionRequestDto electionRequestDto) {
-        Election election = modelMapper.map(electionRequestDto, Election.class);
+    public ResponseEntity<ElectionResponseDto> createElection(@Valid @RequestBody ElectionRequestDto electionRequestDto) {
+        Election election = myMapper.toElection(electionRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).
-                body(modelMapper.map(electionService.createElection(election), ElectionResponseDto.class));
+                body(myMapper.toElectionResponseDto(electionService.createElection(election)));
 
     }
 
-    @PutMapping("/elections/{id}")
-    public ResponseEntity<ElectionResponseDto> updateStatus(@PathVariable Long id){
+    @GetMapping("/elections")
+    public ResponseEntity <List<ElectionResponseDto>> getAllElections(){
+        List<Election> elections = electionService.getAllElections();
 
+        if (elections.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(elections.stream()
+                .map(myMapper::toElectionResponseDto)
+                .toList(), HttpStatus.OK);
     }
+
+    @GetMapping("/elections/{id}")
+    public ResponseEntity<ElectionResponseDto> getElectionById(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).
+                body(myMapper.toElectionResponseDto(electionService.getElectionById(id)));
+    }
+//    @PutMapping("/elections/{id}")
+//    public ResponseEntity<ElectionResponseDto> updateStatus(@PathVariable Long id){
+//
+//    }
 }
