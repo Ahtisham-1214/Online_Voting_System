6ecf2be2294a7f48cb992ca131db2fc8914118dd
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-17 18:46:55 +0500

    added more method accroding to endpoints

--- src/main/java/voting/system/VotingManagementSystem/service/ElectionService.java
+++ src/main/java/voting/system/VotingManagementSystem/service/ElectionService.java
@@ -2,8 +2,13 @@ package voting.system.VotingManagementSystem.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import voting.system.VotingManagementSystem.dto.ElectionUpdateDto;
 import voting.system.VotingManagementSystem.entity.Election;
+import voting.system.VotingManagementSystem.exception.ResourceNotFoundException;
 import voting.system.VotingManagementSystem.repository.ElectionRepository;
+
+import java.util.List;
 
 @Service
 public class ElectionService {
@@ -19,6 +24,36 @@ public class ElectionService {
         return electionRepository.save(election);
     }
 
+    public Election getElectionById(Long id) {
+        return electionRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Election with id " + id + " not found", Election.class.getSimpleName()));
+    }
+
+    public List<Election> getAllElections() {
+        return electionRepository.findAll();
+    }
+
+    @Transactional
+    public Election updateElectionById(Long id, ElectionUpdateDto dto) {
+        Election election = electionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Election not found", Election.class.getSimpleName()));
+
+        if (dto.getTitle() != null) {
+            if (dto.getTitle().isBlank()) throw new IllegalArgumentException("Election Title cannot be empty");
+            election.setTitle(dto.getTitle());
+        }
+
+        if (dto.getMaxPartyMembers() != 0 && dto.getMaxPartyMembers() < 0) {
+            throw new IllegalArgumentException("Max Party Members cannot be negative");
+        }
+
+
+
+        election.setMaxPartyMembers(dto.getMaxPartyMembers());
+        if (dto.getElectionStatus() != null) election.setElectionStatus(dto.getElectionStatus());
+        if (dto.getStartTime() != null) election.setStartTime(dto.getStartTime());
+        if (dto.getEndTime() != null) election.setEndTime(dto.getEndTime());
+        return electionRepository.save(election);
+    }
 
 
 }
