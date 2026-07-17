31c8bb3555b3d1b470ba30b0100d0fda5cc7dc27
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-17 18:45:46 +0500

    added min value for part members restriction

--- src/main/java/voting/system/VotingManagementSystem/dto/ElectionRequestDto.java
+++ src/main/java/voting/system/VotingManagementSystem/dto/ElectionRequestDto.java
@@ -1,5 +1,6 @@
 package voting.system.VotingManagementSystem.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,5 +19,6 @@ public class ElectionRequestDto {
     ElectionStatus electionStatus;
     LocalDateTime startTime;
     LocalDateTime endTime;
+    @Min(value = 0, message = "Max Party Members cannot be negative")
     int maxPartyMembers;
 }
