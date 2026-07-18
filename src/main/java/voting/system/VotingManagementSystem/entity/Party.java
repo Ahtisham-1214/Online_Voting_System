963f4f48ff4e61542e2515c11a204023325a0554
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-18 19:21:54 +0500

    added getter/setter

--- src/main/java/voting/system/VotingManagementSystem/entity/Party.java
+++ src/main/java/voting/system/VotingManagementSystem/entity/Party.java
@@ -1,8 +1,12 @@
 package voting.system.VotingManagementSystem.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Party {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
