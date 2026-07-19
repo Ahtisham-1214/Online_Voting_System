e218f5e1272fb9ed0383f5ee5d845695d9d91431
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-19 20:12:07 +0500

    updated mapper

--- src/main/java/voting/system/VotingManagementSystem/mapper/MyMapper.java
+++ src/main/java/voting/system/VotingManagementSystem/mapper/MyMapper.java
@@ -1,7 +1,6 @@
 package voting.system.VotingManagementSystem.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import voting.system.VotingManagementSystem.dto.*;
 import voting.system.VotingManagementSystem.entity.Election;
 import voting.system.VotingManagementSystem.entity.Party;
@@ -13,8 +12,8 @@ public interface MyMapper {
     ElectionResponseDto toElectionResponseDto(Election election);
     Election toElection(ElectionRequestDto electionRequestDto);
 
-    StudentDto toStudentDto(Student student);
-    Student toStudent(StudentDto studentDto);
+    StudentResponseDto toStudentResponseDto(Student student);
+    Student toStudent(StudentRequestDto studentRequestDto);
 
     Party toParty(PartyRequestDto partyRequestDto);
     PartyResponseDto toPartyResponseDto(Party party);
