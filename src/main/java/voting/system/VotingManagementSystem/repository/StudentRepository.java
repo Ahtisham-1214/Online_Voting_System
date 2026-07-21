09e59c4e1395b2dba32f61ffd259bb3c415f836e
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-21 22:16:03 +0500

    added optional interface to handle custom exception

--- src/main/java/voting/system/VotingManagementSystem/repository/StudentRepository.java
+++ src/main/java/voting/system/VotingManagementSystem/repository/StudentRepository.java
@@ -1,15 +1,18 @@
 package voting.system.VotingManagementSystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import voting.system.VotingManagementSystem.entity.Student;
 
+import java.util.Optional;
+
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    Student findByEmail(String email);
+public interface StudentRepository extends JpaRepository<Student, Long>, JpaSpecificationExecutor<Student> {
 
-    Student findBySapId(String sapId);
+    Optional<Student> findByEmail(String email);
 
+    Optional<Student> findBySapId(String sapId);
 
 
 }
