43533afcfd02c4a1e9494ecf3b93a78a3731171c
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-26 22:16:29 +0500

    added more exception handling

--- src/main/java/voting/system/VotingManagementSystem/exception/GlobalExceptionHandler.java
+++ src/main/java/voting/system/VotingManagementSystem/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package voting.system.VotingManagementSystem.exception;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import voting.system.VotingManagementSystem.service.PartyMemberService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,6 +37,19 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(errorPayload, HttpStatus.BAD_REQUEST);
     }
+
+
+    @ExceptionHandler(CSVFileException.class)
+    public ResponseEntity<ErrorResponse> CSVFileException(CSVFileException ex) {
+        ErrorResponse errorPayload = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Bad Argument",
+                ex.getMessage(),
+                ex.getClassName()
+        );
+        return new ResponseEntity<>(errorPayload, HttpStatus.BAD_REQUEST);
+    }
+
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
@@ -83,4 +99,37 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorPayload, HttpStatus.BAD_REQUEST);
     }
 
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException ex) {
+        ErrorResponse errorPayload = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation Error",
+                ex.getMessage(),
+                PartyMemberService.class.getSimpleName() // kaam chla raha hun because for this exception I must have created a class
+
+        );
+
+        return new ResponseEntity<>(errorPayload, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolation(ConstraintViolationException ex) {
+        String errorMessage = ex.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .findFirst()
+                .orElse("Invalid request parameter");
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(), // "Bad Request"
+                errorMessage,                         // Exactly "Invalid Sap Id"
+                ex.getClass().getSimpleName()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+
+    }
 }
