ab165c29a93dc8544d90e62e3d8a26f8e0c22dda
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-27 21:54:23 +0500

    for handling party member logic

---
+++ src/main/java/voting/system/VotingManagementSystem/service/PartyMemberService.java
@@ -0,0 +1,76 @@
+package voting.system.VotingManagementSystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import voting.system.VotingManagementSystem.dto.PartyMemberRequestDto;
+import voting.system.VotingManagementSystem.entity.Party;
+import voting.system.VotingManagementSystem.entity.PartyMember;
+import voting.system.VotingManagementSystem.entity.Student;
+import voting.system.VotingManagementSystem.entity.StudentStatus;
+import voting.system.VotingManagementSystem.exception.ResourceNotFoundException;
+import voting.system.VotingManagementSystem.repository.PartyMemberRepository;
+import voting.system.VotingManagementSystem.repository.PartyRepository;
+import voting.system.VotingManagementSystem.repository.StudentRepository;
+
+@Service
+public class PartyMemberService {
+    private final StudentRepository studentRepository;
+    private final PartyRepository partyRepository;
+    private final PartyMemberRepository partyMemberRepository;
+
+    @Autowired
+    public PartyMemberService(StudentRepository studentRepository,
+                              PartyMemberRepository partyMemberRepository,
+                              PartyRepository partyRepository) {
+        this.studentRepository = studentRepository;
+        this.partyMemberRepository = partyMemberRepository;
+        this.partyRepository = partyRepository;
+    }
+
+    /*
+     * There are few things to check before creating a PartyMember:
+     * 1) There must exist a Party with the given ID
+     * 2) The student must exist and be enrolled
+     * 3) The student must neither be already a member of a party nor a member of a different election meaning that it can only participate in one election
+     * 4) A party can't have more than one member of same designation like only 1 president, 1 GS, 1 VP
+     */
+    public PartyMember addPartyMember(PartyMemberRequestDto partyMemberRequestDto) {
+
+
+
+        Party party = partyRepository.findById(partyMemberRequestDto.getPartyId()).orElseThrow(
+                ()-> new ResourceNotFoundException("Party doesn't exists with party id: " + partyMemberRequestDto.getPartyId(),
+                        PartyMemberService.class.getSimpleName() )
+        );
+
+        Student student = studentRepository.findBySapId(partyMemberRequestDto.getSapId())
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found with ID: " + partyMemberRequestDto.getSapId(),
+                        PartyMemberService.class.getSimpleName()));
+
+
+        if (student.getStudentStatus() != StudentStatus.ENROLLED) {
+            throw new IllegalStateException("Student is not Enrolled and cannot participate.");
+        }
+
+        if(partyMemberRepository.existsByStudentId(student.getId()))
+            throw new IllegalStateException("Student with Sap ID: "+ student.getSapId() +" is already a member of " + partyMemberRepository.findPartyByStudentId(student.getId())); // making sure one student can participate in an election only once
+
+
+        if (partyMemberRepository.existsByPartyIdAndPosition(party.getId(), partyMemberRequestDto.getPosition())) {
+            throw new IllegalArgumentException(party.getName() + " already has a " + partyMemberRequestDto.getPosition()
+            );
+        }
+
+
+
+        // Create and save the PartyMember
+        PartyMember member = new PartyMember();
+        member.setStudent(student);
+        member.setParty(party);
+        member.setPosition(partyMemberRequestDto.getPosition());
+
+
+        return partyMemberRepository.save(member);
+    }
+
+}
