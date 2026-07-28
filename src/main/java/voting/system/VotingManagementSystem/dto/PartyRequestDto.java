a2b746e80a9f458ff945941f8c42c490be3a7a12
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-28 18:32:03 +0500

    added validation for party name

--- src/main/java/voting/system/VotingManagementSystem/dto/PartyRequestDto.java
+++ src/main/java/voting/system/VotingManagementSystem/dto/PartyRequestDto.java
@@ -2,6 +2,8 @@ package voting.system.VotingManagementSystem.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,7 +13,9 @@ import lombok.Setter;
 @NoArgsConstructor
 public class PartyRequestDto {
 
-    @NotBlank( message = "Party Name cannot be empty")
+    @NotNull(message = "Party name cannot be null")
+    @NotBlank(message = "Party Name cannot be empty")
+    @Pattern(regexp = "^[A-Za-z ]+$", message = "No special character or number allowed")
     private String name;
     private String slogan;
 }
--- src/main/java/voting/system/VotingManagementSystem/dto/PartyUpdateDto.java
+++ src/main/java/voting/system/VotingManagementSystem/dto/PartyUpdateDto.java
@@ -1,5 +1,6 @@
 package voting.system.VotingManagementSystem.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class PartyUpdateDto {
+    @Pattern(regexp = "^[A-Za-z ]+$", message = "No special character or number allowed")
     private String name;
     private String slogan;
 }
