719750b8cea31108370621a4b291ad607b39a259
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-28 18:33:16 +0500

    added queries to check duplication of name/slogan

--- src/main/java/voting/system/VotingManagementSystem/repository/PartyRepository.java
+++ src/main/java/voting/system/VotingManagementSystem/repository/PartyRepository.java
@@ -1,9 +1,15 @@
 package voting.system.VotingManagementSystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import voting.system.VotingManagementSystem.entity.Party;
 
 @Repository
 public interface PartyRepository extends JpaRepository<Party, Long> {
+    boolean existsByName(String name);
+
+    @Query("select (count(p) > 0) from Party p where p.slogan = :slogan")
+    boolean existBySlogan(@Param("slogan") String slogan);
 }
