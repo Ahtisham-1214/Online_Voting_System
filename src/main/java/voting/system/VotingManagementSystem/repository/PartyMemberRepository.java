025e796e65bb1c09ae8074757fc9fb92fdfa80f6
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-30 17:31:07 +0500

    party member repo

---
+++ src/main/java/voting/system/VotingManagementSystem/repository/PartyMemberRepository.java
@@ -0,0 +1,34 @@
+package voting.system.VotingManagementSystem.repository;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import voting.system.VotingManagementSystem.entity.PartyMember;
+import voting.system.VotingManagementSystem.entity.Position;
+
+import java.util.List;
+
+@Repository
+public interface PartyMemberRepository extends JpaRepository<PartyMember, Long> {
+    Boolean existsByStudentId(Long studentId);
+
+    boolean existsByPartyIdAndPosition(long id, @NotNull(message = "Position is required") Position position);
+
+    @Query("SELECT p.name FROM PartyMember pm JOIN pm.party p WHERE pm.student.id = :id ")
+    String findPartyByStudentId(@Param("id") Long id);
+
+//    @Query("SELECT pm FROM PartyMember pm " +
+//            "JOIN FETCH pm.student " +
+//            "JOIN FETCH pm.party " +
+//            "WHERE pm.party.id = :partyId")
+//    List<PartyMember> findPartyMembersById(@Param("partyId") Long partyId, Pageable pageable);
+
+    @Query("SELECT pm FROM PartyMember pm " +
+            "JOIN FETCH pm.student " +
+            "JOIN FETCH pm.party " +
+            "WHERE pm.party.id = :partyId AND (:position IS NULL OR pm.position = :position)")
+    List<PartyMember> findPartyMembersById(@Param("partyId") Long partyId,@Param("position") Position position, Pageable pageable);
+}
