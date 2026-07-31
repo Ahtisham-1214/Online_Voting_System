115d5cc453c05e37edcb54408d19e4157b1adcd8
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-07-31 19:06:33 +0500

    update mapper

--- src/main/java/voting/system/VotingManagementSystem/mapper/MyMapper.java
+++ src/main/java/voting/system/VotingManagementSystem/mapper/MyMapper.java
@@ -1,9 +1,11 @@
 package voting.system.VotingManagementSystem.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import voting.system.VotingManagementSystem.dto.*;
 import voting.system.VotingManagementSystem.entity.Election;
 import voting.system.VotingManagementSystem.entity.Party;
+import voting.system.VotingManagementSystem.entity.PartyMember;
 import voting.system.VotingManagementSystem.entity.Student;
 
 @Mapper(componentModel = "spring")
@@ -15,6 +17,14 @@ public interface MyMapper {
     StudentResponseDto toStudentResponseDto(Student student);
     Student toStudent(StudentRequestDto studentRequestDto);
 
+    @Mapping(source = "electionId", target = "election.id")
     Party toParty(PartyRequestDto partyRequestDto);
+    @Mapping(source = "election.id", target = "electionId")
     PartyResponseDto toPartyResponseDto(Party party);
+
+    @Mapping(source = "party.id", target = "partyId")
+    @Mapping(source = "party.name", target = "partyName")
+    @Mapping(source = "student.sapId", target = "sapId")
+    @Mapping(source = "student.name", target = "studentName")
+    PartyMemberResponseDto toPartyMemberResponseDto(PartyMember partyMember);
 }
