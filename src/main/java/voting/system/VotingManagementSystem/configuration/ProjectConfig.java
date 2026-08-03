a4aa8dd18dee6f93213c641e4cb8a23cf7c6af0a
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-08-03 08:02:32 +0500

    updated config

--- src/main/java/voting/system/VotingManagementSystem/configuration/ProjectConfig.java
+++ src/main/java/voting/system/VotingManagementSystem/configuration/ProjectConfig.java
@@ -1,13 +1,9 @@
 package voting.system.VotingManagementSystem.configuration;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProjectConfig {
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+
 }
