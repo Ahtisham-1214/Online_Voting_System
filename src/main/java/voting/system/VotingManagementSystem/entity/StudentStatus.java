c460c7643eb1d0cfe018ccf169e3d18a66ce96df
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-08-03 07:59:55 +0500

    student current status

--- src/main/java/voting/system/VotingManagementSystem/entity/StudentStatus.java
+++ src/main/java/voting/system/VotingManagementSystem/entity/StudentStatus.java
@@ -1,6 +1,26 @@
 package voting.system.VotingManagementSystem.entity;
 
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import voting.system.VotingManagementSystem.exception.IllegalEnumException;
+
+import java.util.Arrays;
+@JsonFormat(shape = JsonFormat.Shape.STRING, with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 public enum StudentStatus {
-    ACTIVE,
-    DROPPED
+    ENROLLED,
+    DROPPED;
+
+    @JsonCreator
+    public static StudentStatus fromString(String value) {
+        if (value == null || value.isEmpty()) {
+            return null;
+        }
+        try {
+            return StudentStatus.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalEnumException(value + " is not accepted. Allowed: " +
+                    Arrays.toString(StudentStatus.values()), StudentStatus.class.getSimpleName());
+        }
+    }
 }
