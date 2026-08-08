c899c79d1b647b9ef83d20646b0e6c962cefce1f
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-08-08 13:14:54 +0500

    added time validation methods

--- src/main/java/voting/system/VotingManagementSystem/util/HelperClass.java
+++ src/main/java/voting/system/VotingManagementSystem/util/HelperClass.java
@@ -1,8 +1,16 @@
 package voting.system.VotingManagementSystem.util;
 
+import voting.system.VotingManagementSystem.entity.Election;
+import voting.system.VotingManagementSystem.entity.ElectionStatus;
 import voting.system.VotingManagementSystem.entity.Position;
 
+import java.time.LocalDateTime;
+
 public class HelperClass {
+
+    private HelperClass(){
+
+    }
 
     public static boolean isExPosition(Position position){
 
@@ -11,4 +19,26 @@ public class HelperClass {
                 position == Position.EX_TREASURER);
     }
 
+    public static void validateTimes(LocalDateTime startTime, LocalDateTime endTime) {
+        if (startTime == null || endTime == null) return;
+        if (endTime.isBefore(startTime) || endTime.isEqual(startTime)) {
+            throw new IllegalArgumentException("End time must be after start time");
+        }
+    }
+
+    public static void autoDetermineStatus(Election election) {
+        if (election.getStartTime() == null || election.getEndTime() == null) {
+            return;
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+
+        if (now.isBefore(election.getStartTime())) {
+            election.setElectionStatus(ElectionStatus.DRAFT);
+        } else if (!now.isBefore(election.getStartTime()) && now.isBefore(election.getEndTime())) {
+            election.setElectionStatus(ElectionStatus.VOTING);
+        } else {
+            election.setElectionStatus(ElectionStatus.CLOSED);
+        }
+    }
 }
